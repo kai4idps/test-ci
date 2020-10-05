e1b0ca39ea0e04500f71ba99a5d717040b7db8e3
kai4idps <kai@4idps.com>
2020-10-05 14:44:58 +0800

    CI

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit3 <code>src/App.js</code> and save to reload.
+          Edit33 <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
