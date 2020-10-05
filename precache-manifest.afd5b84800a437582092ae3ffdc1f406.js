e9980e8dd567ddcdb0e862de40b476e6b635961f
kai4idps <kai@4idps.com>
2020-10-05 10:15:28 +0800

    Updates

---
+++ precache-manifest.afd5b84800a437582092ae3ffdc1f406.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd6717bbe2ed59b9959c590d47eee448",
+    "url": "/test-ci/index.html"
+  },
+  {
+    "revision": "dafeca8f3fd69d15eb45",
+    "url": "/test-ci/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e198a47f4a8c4b791545",
+    "url": "/test-ci/static/js/2.2eec847f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-ci/static/js/2.2eec847f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dafeca8f3fd69d15eb45",
+    "url": "/test-ci/static/js/main.fa2ad6f9.chunk.js"
+  },
+  {
+    "revision": "033629d57089b171a0b6",
+    "url": "/test-ci/static/js/runtime-main.5f70ae58.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-ci/static/media/logo.5d5d9eef.svg"
+  }
+]);
